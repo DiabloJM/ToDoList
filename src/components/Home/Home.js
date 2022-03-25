76fb8ce90e9aae7d52a6309c5be490438f5b3940
LAPTOP-5835AAD2\ferna <fernandojmichel@outlook.com>
2022-03-25 11:32:57 -0600

    New Styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
+
     render() {
+        const buttonStyle={
+            backgroundColor:'gray',
+            border: '1px solid red',
+        }
+
         return (
-            <div>
-                
+            <div className="Home">
+              <h1>Home</h1>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Rick Roll</a>
+            
+              <div>
+                  {/*Styles inline*/}
+                    <button style={{
+                        backgroundColor:'red',
+                        border: '1px solid black',
+                    }}>
+                        Click
+                    </button>
+                    {/*Styles objects */}
+                    <button style={buttonStyle}>
+                        Click
+                    </button>
+                </div>  
             </div>
         );
     }
