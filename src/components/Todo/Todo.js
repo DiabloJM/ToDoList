4701921fe4643550451f25293ede545b720ddac0
LAPTOP-5835AAD2\ferna <fernandojmichel@outlook.com>
2022-03-28 13:28:21 -0600

    Check and Delete Task

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import List from './List'
 import {v4 as uuidv4} from 'uuid';
 
 class Todo extends Component {
@@ -29,12 +30,80 @@ class Todo extends Component {
         }
     }
 
+    handleOnChange = e => {
+        const {target:{value}} = e;
+
+        this.setState({
+            task: value
+        })
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault();
+
+        if(this.state.task.trim() !== ''){
+            this.setState({
+                task: '',
+                items: [
+                    ...this.state.items,
+                    {
+                        id: uuidv4(),
+                        task: this.state.task,
+                        complete: false
+                    }
+                ]
+            })
+        }
+    }
+
+    markAsCompleted = id => {
+        const {items} = this.state;
+
+        const foundTask = items.find(
+            item => item.id === id
+        );
+
+        foundTask.complete = true;
+
+        this.setState({
+            items: [
+                ...this.state.items,
+            ]
+        })
+    }
+
+    removeTask = id => {
+        const {items} = this.state;
+        const filteredItems = items.filter(
+            item => item.id !== id
+        );
+
+        this.setState({
+            items: filteredItems,
+        })
+    }
+
     render() {
 
         console.log(this.state.items);
+        //console.log(this.state.task);
         return (
             <div className="Todo">
                <h1>Nueva Tarea</h1> 
+
+               <form onSubmit={this.handleOnSubmit}>
+                    <input 
+                        type = "text"
+                        value = {this.state.task}
+                        onChange = {this.handleOnChange}
+                    />
+               </form>
+
+               <List 
+                    items = {this.state.items}
+                    markAsCompleted = {this.markAsCompleted}
+                    removeTask = {this.removeTask}
+                />
             </div>
         );
     }
